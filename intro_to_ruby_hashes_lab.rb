0fe7f90c59af52786185449a13adb280668f9c48
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-05 19:35:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,8 @@
 def base_hash
   monopoly = {
-		:railroads =>
+		:railroads => {}
 	}# Remember implic it return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	{}
 end
 
 def monopoly_with_second_tier
